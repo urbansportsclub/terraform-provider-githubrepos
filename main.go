f45fd2b70916e797aafdcf4409862fa1b43a80a9
Onur Guzel <onur.guzel@urbansportsclub.com>
2023-11-17 14:17:48 +0100

    Remove left over HashiCorp references

--- main.go
+++ main.go
@@ -35,13 +35,7 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// NOTE: This is not a typical Terraform Registry provider address,
-		// such as registry.terraform.io/urbansportsclub/githubrepos. This specific
-		// provider address is used in these tutorials in conjunction with a
-		// specific Terraform CLI configuration for manual development testing
-		// of this provider.
-		Address: "hashicorp.com/edu/githubrepos",
-		// Address: "registry.terraform.io/urbansportsclub/githubrepos",
+		Address: "registry.terraform.io/urbansportsclub/githubrepos",
 		Debug: debug,
 	}
 
